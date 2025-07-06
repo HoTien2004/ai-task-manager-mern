c5af94c5579931be287da4dd59d0f0e7d6ed3ac4
QUYẾT TIẾN <152900541+HoTien2004@users.noreply.github.com>
2025-07-07 00:10:02 +0700

    Refactor code structure for improved readability and maintainability

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const path = require('path');
 const connectDB = require('./config/db');
 
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+
 const app = express();
 
 // Middleware to handle CORS
@@ -21,11 +24,14 @@ connectDB();
 // Middleware
 app.use(express.json());
 
-
+// Check if the server is running
+app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
 
 // Routes
-// app.use('api/auth', authRoutes);
-// app.use('api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 // app.use('api/tasks', taskRoutes);
 // app.use('api/reports', reportRoutes);
 
