2584e2a3452afeb10599753e7970002072762912
QUYẾT TIẾN <152900541+HoTien2004@users.noreply.github.com>
2025-07-08 21:28:05 +0700

    feat: Add Input component with password visibility toggle
    feat: Implement ProfilePhotoSelector for user profile image upload and preview
    
    feat: Create AuthLayout for authentication pages with responsive design
    
    feat: Establish UserContext for user authentication state management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,31 +15,34 @@ import PrivateRoute from './routes/PrivateRoute'
 import UserDashboard from './pages/User/UserDashboard'
 import MyTasks from './pages/User/MyTasks'
 import ViewTaskDetails from './pages/User/ViewTaskDetails'
+import UserProvider from './context/userContext'
 
 const App = () => {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/sign-up" element={<SignUp />} />
+      <UserProvider>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
 
-          {/* Admin Routes */}
-          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
-            <Route path="/admin/dashboard" element={<Dashboard />} />
-            <Route path="/admin/create-task" element={<CreateTask />} />
-            <Route path="/admin/tasks" element={<ManageTasks />} />
-            <Route path="/admin/users" element={<ManageUsers />} />
-          </Route>
+            {/* Admin Routes */}
+            <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+              <Route path="/admin/dashboard" element={<Dashboard />} />
+              <Route path="/admin/create-task" element={<CreateTask />} />
+              <Route path="/admin/tasks" element={<ManageTasks />} />
+              <Route path="/admin/users" element={<ManageUsers />} />
+            </Route>
 
-          {/* User Routes */}
-          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
-            <Route path="/user/dashboard" element={<UserDashboard />} />
-            <Route path="/user/tasks" element={<MyTasks />} />
-            <Route path="/user/task-details/:id" element={<ViewTaskDetails />} />
-          </Route>
-        </Routes>
-      </Router>
+            {/* User Routes */}
+            <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+              <Route path="/user/dashboard" element={<UserDashboard />} />
+              <Route path="/user/tasks" element={<MyTasks />} />
+              <Route path="/user/task-details/:id" element={<ViewTaskDetails />} />
+            </Route>
+          </Routes>
+        </Router>
+      </UserProvider>
     </>
   )
 }
